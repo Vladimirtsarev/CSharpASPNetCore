c5aa517cf87055d651705e41677bf59e5063e7b1
Владимир <vladimirtsarev@ya.ru>
2020-08-31 15:24:48 +0300

    Добавлены статические ресурсы

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,8 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
