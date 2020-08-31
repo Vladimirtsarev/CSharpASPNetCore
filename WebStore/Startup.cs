43d7f2449c284b11ee7449ae7e1aa2e99f674866
Владимир <Vladimirtsarev@ya.ru>
2020-08-31 21:08:01 +0300

    Тест

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -24,7 +24,7 @@ namespace WebStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc();//
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
