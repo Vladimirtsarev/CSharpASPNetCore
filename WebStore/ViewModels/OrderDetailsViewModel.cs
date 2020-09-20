2edf4a17242a39da48fe4506b9fb6fb59e2e149c
Владимир <Vladimirtsarev@ya.ru>
2020-09-20 20:43:50 +0300

    Area

---
+++ WebStore/ViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStore.ViewModels
+{
+    public class OrderDetailsViewModel
+    {
+        public CartViewModel CartViewModel { get; set; }
+        public OrderViewModel OrderViewModel { get; set; }
+    }
+}
